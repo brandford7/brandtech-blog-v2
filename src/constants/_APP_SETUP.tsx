6d9e71de7ead5cf42fc11235a47f4b23972a55c7
Brandford Junior Korang-Teku <66058674+brandford7@users.noreply.github.com>
2024-01-04 16:15:13 +0000

    Update _APP_SETUP.tsx
    edited keywords in constants folder

--- src/constants/_APP_SETUP.tsx
+++ src/constants/_APP_SETUP.tsx
@@ -7,6 +7,6 @@ export const LOGO:string="Syeda Maham Fahim"
 
 export const META_DESCRIPTION= `Hey! This is my personal blog website where I decode the mysteries of software development and explore cutting-edge tech innovations.`
 
-export const META_SEO_KEYWORDS="technology, coding, web development, data engineering, javascript, data science, AWS, cloud, blockchain, insights, Git, GitHub, app development, AWS cloud solutions, data science"
+export const META_SEO_KEYWORDS="technology, coding, web development, css, javascript, Node js, Expressjs data science, React js,Next js,Typescript, cloud, blockchain, insights, Git, GitHub, app development"
 
 export const META_CANONICAL=''
